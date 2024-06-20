11974bfa7bcd9856753577cdf398d7eed1535b82
pc <pc@DESKTOP-9RJ34RI>
2024-06-20 15:42:01 +0700

    WorkDispatch

--- WorkManagementSystem/Features/WorkDispatch/GetWorkDispatchById/Endpoint.cs
+++ WorkManagementSystem/Features/WorkDispatch/GetWorkDispatchById/Endpoint.cs
@@ -1,6 +1,22 @@
 ﻿namespace WorkManagementSystem.Features.WorkDispatch.GetWorkDispatchById
 {
-    public class Endpint
+    public class Endpoint : Endpoint<Request, ResultModel<WorkDispatchDetailResponse>>
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public Endpoint(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public override void Configure()
+        {
+            AllowAnonymous();
+            Get("WorkDispatch/get-work-dispatch-by-id");
+        }
+
+        public override async Task HandleAsync(Request query, CancellationToken c)
+        {
+            var data = new Data(_unitOfWork);
+            await SendAsync(await data.GetWorkItemById(query));
+        }
     }
 }
