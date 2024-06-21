9b06ca12fc9228285376e840e78833f511a10fc7
Văn Sỹ <vansy9x@gmail.com>
2024-06-21 14:11:48 +0700

    Commit code

--- WorkManagementSystem/Features/WorkDispatch/AddUserFollow/Endpoint.cs
+++ WorkManagementSystem/Features/WorkDispatch/AddUserFollow/Endpoint.cs
@@ -1,6 +1,6 @@
-﻿namespace WorkManagementSystem.Features.WorkDispatch.AddUserFollow
+﻿namespace WorkManagementSystem.Features.WorkDispatch.AddUserFollow;
+
+// Chức năng chuyển công văn cho người theo dõi (bật popup lên cho chọn người theo dõi công văn và có thêm phần vai trò:  Người theo dõi, Người phối hợp/ xử lý)
+public class Endpoint
 {
-    public class Endpoint
-    {
-    }
 }
--- WorkManagementSystem/Features/WorkDispatch/AddUserFollow/Models.cs
+++ WorkManagementSystem/Features/WorkDispatch/AddUserFollow/Models.cs
@@ -1,6 +1,12 @@
-﻿namespace WorkManagementSystem.Features.WorkDispatch.AddUserFollow
+﻿namespace WorkManagementSystem.Features.WorkDispatch.AddUserFollow;
+
+public class Request
 {
-    public class Models
-    {
-    }
+    public Guid UserId { get; set; }
+    public FollowUserType FollowUserType { get; set; }
+}
+public enum FollowUserType
+{
+    Follow = 1,
+    Combination = 2
 }
