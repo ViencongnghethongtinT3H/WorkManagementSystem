ff3d1e1d38e963584d53ac61f82df7e16de294de
hoangphampvh <hoangpham19112002@gmail.com>
2024-06-24 23:43:21 +0700

    api create workFlow step

---
+++ WorkManagementSystem/Features/WorkflowStep/Models.cs
@@ -0,0 +1,15 @@
+﻿namespace WorkManagementSystem.Features.WorkStep
+{
+    public class Request
+    {
+        public Guid WorkflowId { get; set; }
+        public StepEnum Step { get; set; }
+        public Guid UserConfirm { get; set; } // người xử lý của bước đấy
+        public string? Note { get; set; }
+    }
+    public class Response
+    {
+        public string Message => "Work fllow step saved!";
+        public object result { get; set; }
+    }
+}
