dc59ee0a18028e5a49fd747f2699b4fe369dc235
hoangphampvh <hoangpham19112002@gmail.com>
2024-06-26 16:47:55 +0700

    git commit

--- WorkManagementSystem/Features/WorkDispatch/AddUserToWorkDispatch/Data.cs
+++ WorkManagementSystem/Features/WorkDispatch/AddUserToWorkDispatch/Data.cs
@@ -58,7 +58,7 @@
                 {
                     Data = true,
                     Status = 200,
-                    ErrorMessage = "Thêm người theo dõi thành công!",
+                    ErrorMessage = "Thêm thành công!",
                     IsError = false,
                 };
             }
