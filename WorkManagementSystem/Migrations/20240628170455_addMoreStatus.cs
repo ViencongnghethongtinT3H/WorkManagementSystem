40285ead060649a7b80c2e4d20825e3df0826865
Văn Sỹ <vansy9x@gmail.com>
2024-06-29 00:05:41 +0700

    Commit code

---
+++ WorkManagementSystem/Migrations/20240628170455_addMoreStatus.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkManagementSystem.Migrations
+{
+    public partial class addMoreStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserWorkflowStatus",
+                table: "UserWorkflows",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserWorkflowStatus",
+                table: "UserWorkflows");
+        }
+    }
+}
